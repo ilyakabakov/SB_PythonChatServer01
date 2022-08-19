9cdcfff0d8471b317043fee07d71e096e9b888de
ilyakabakov <ikabakov90@list.ru>
2022-08-19 12:50:59 +0700

    Project from Skill Box Python intensive Simple chat messanger Uses: json database web-interface with Flask
    New:
    -added message counter
    -some issues in README.md
    - XSS issue fixed(solution prompted by @skraynev)

--- messenger_script.py
+++ messenger_script.py
@@ -1,5 +1,6 @@
 from datetime import datetime as dt
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, escape, abort
+from typing import Tuple
 import json
 
 app = Flask(__name__)  # create new app(__name__ is constant)
@@ -7,7 +8,7 @@ app = Flask(__name__)  # create new app(__name__ is constant)
 DB_FILE = "db.json"
 
 
-def load_messages():
+def load_messages() -> list:
     with open(DB_FILE, "r", encoding='utf-8') as json_file:
         data = json.load(json_file)
     return data["messages"]
@@ -17,12 +18,11 @@ all_messages = load_messages()  # список всех сообщений в м
 
 
 @app.route("/message_count")
-def messages_count():
-    m_count = len(all_messages)
-    return {"counter": m_count}
+def messages_count() -> str:
+    return f"Messages in chat: {str(len(all_messages))}"
 
 
-def save_messages():
+def save_messages() -> None:
     data = {
         "messages": all_messages
     }
@@ -31,20 +31,19 @@ def save_messages():
 
 
 @app.route("/")  # main page
-def index_page():
-    return 'From <b>SKILLBOX</b>'
+def index_page() -> str:
+    return "From <b>SKILLBOX</b>!"
 
 
 # all messages list:
-# flask сам кодирует в json если функцию возвращает словарь
 @app.route("/get_messages")  # all messages page
-def get_message():
+def get_message() -> dict:
     return {"messages": all_messages}
 
 
 # messages output func
-def print_message(message):
-    print(f"[{message['sender']}]: {message['text']} / {message['time']}")
+# def print_message(message):
+#     print(f"[{message['sender']}]: {message['text']} / {message['time']}")
 
 
 @app.route("/chat")
@@ -52,40 +51,53 @@ def display_chat():
     return render_template("form.html")
 
 
+def length_check(name_key: str, text_value: str, min_val: int, max_val: int) -> None:
+    if not text_value:
+        raise ValueError(f"Not valid  {name_key}. Name can can contain from 3 to 100 characters!")
+    else:
+        current_len = len(text_value)
+        if max_val < current_len or current_len < min_val:
+            raise ValueError(
+                f"ERROR in {name_key} pole."
+                f"{name_key} can can contain from {min_val} to {max_val} characters! "
+                f"The current length = {current_len}."
+            )
+
+
+def checker(request_data: dict) -> Tuple[str, str]:
+    sender = escape(request_data.get('name'))
+    length_check('name', sender, 3, 100)
+
+    text = escape(request_data['text'])
+    length_check('text', text, 1, 3000)
+
+    return sender, text
+
+
 # send message получает из запроса имя отправителя и текст
 @app.route("/send_message")
-def send_message():
-    sender = request.args["name"]
-    text = request.args["text"]
+def send_message() -> str:
+    try:
+        sender, text = checker(request.args)
+    except ValueError:
+        abort(400)
     add_message(sender, text)
-    checker(sender, text)
     save_messages()
-
     return "OK"
 
 
-def checker(sender, text):
-    checked_d = {
-        'sender': sender,
-        'text': text,
-        'time': dt.now().strftime("%H:%M:%S")
-    }
-    if len(str(sender)) < 3 or len(str(sender)) > 100:
-        checked_d['sender'] = 'ERROR'
-        checked_d['text'] = 'Write the valid Name: The name should be at least 3 characters and not more than 100'
-    if len(str(text)) < 1 or len(str(text)) > 3000:
-        checked_d['text'] = 'ERROR: Your text so much loOoong or short. And will deleted when i finish the code!'
-    all_messages.append(checked_d)
-
-
 # incoming messages func
-def add_message(sender, text):
+def add_message(sender: str, text: str) -> None:
     new_message = {
         'sender': sender,
         'text': text,
-        'time': dt.now().strftime("%H:%M:%S")
+        'time': dt.now().time().isoformat(timespec='seconds')
     }
     all_messages.append(new_message)  # .append - add in dict
 
+
+# Для каждого сообщения в списке all_messages
+# for message in all_messages:
+#     print_message(message)
 
 app.run()
